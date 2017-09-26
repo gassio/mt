db2eb646315d87d76301cc530cc25dde54391b13
gassio <gassiogi@gmail.com>
2017-09-27 00:34:50 +0300

    Added video date field (not completed)

--- src/components/Pages/Home.vue
+++ src/components/Pages/Home.vue
@@ -32,6 +32,7 @@
 					<div class="videocard-details">
 						<p class="videocard-description">{{ v.title }}</p>
 						<p class="videocard-semester">{{ v.class }}</p>
+						<p class="videocard-date">{{ v.presentedAt }}</p> <!--.substring(0,9) -->
 					</div>
 					<div class="videocard-foot">
 						<span class="videocard-genre"><i class="fa fa-commenting-o" aria-hidden="true"></i> {{ v.genre }}</span>
