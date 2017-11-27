6b610300bdf9dfb40483c9e8d368e9f2804bbeaa
gassio <gassiogi@gmail.com>
2017-11-27 23:25:02 +0200

    Upload video: Fixed duration property bug (parseInt)

--- src/components/Pages/Wiki.vue
+++ src/components/Pages/Wiki.vue
@@ -1,22 +1,12 @@
 <template>
     <div class="wiki container">
         <h1>Coming soon.</h1>
-        <button @click="createVideo()">Create video</button>
         <router-view></router-view>
     </div>
 </template>
 
 <script>
     export default {
-        methods: {
-            createVideo() {
-                this.$store.dispatch('createVideo', {
-                    link: "http://www.google.com", 
-                    duration: 135, 
-                    key: '324key678'
-                })
-            }
-        }
     }
 </script>
 
