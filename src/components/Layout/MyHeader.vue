8868b07e08ba117e83288ba038b686cf30a57a24
gassio <gassiogi@gmail.com>
2018-01-03 14:17:49 +0200

    Navbar menu: Burger

--- src/components/Layout/MyHeader.vue
+++ src/components/Layout/MyHeader.vue
@@ -20,6 +20,12 @@
                     <div class="navbar-item"></div>
                 </div>
             </div>
+
+            <!-- <button class="button navbar-burger">
+                <span></span>
+                <span></span>
+                <span></span>
+            </button> -->
         </div>
     </nav>
 </template>
@@ -32,6 +38,35 @@
         data() {
             return {
             }
+        },
+        mounted() {
+            document.addEventListener('DOMContentLoaded', function () {
+
+                // Get all "navbar-burger" elements
+                var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+                // Check if there are any navbar burgers
+                if ($navbarBurgers.length > 0) {
+
+                    // Add a click event on each of them
+                    $navbarBurgers.forEach(function ($el) {
+                        $el.addEventListener('click', function () {
+
+                            // Get the target from the "data-target" attribute
+                            var target = $el.dataset.target;
+                            var $target = document.getElementById(target);
+                            console.log($target)
+                            console.log($el)
+
+                            // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+                            $el.classList.toggle('is-active');
+                            $target.classList.toggle('is-active');
+
+                        });
+                    });
+                }
+
+            });
         },
         methods: {
             // Setting uploading video dialog state as true.
