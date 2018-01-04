406d4a3a351f6f0c6e69470715a27cc9293bd91e
gassio <gassiogi@gmail.com>
2018-01-04 19:02:25 +0200

    Admin ui:  bulma layout

--- src/components/Admin/AdminPage.vue
+++ src/components/Admin/AdminPage.vue
@@ -1,55 +1,98 @@
 <template>
-    <div class= "admin container">
-
-		<div class="admin__head">
-			<div class= "columns is-mobile is-gapless is-marginless">
-				<div class= "column is-8">
-					<!--<div class= "admin__head-semester">
-						<button id="all" class="admin__head-semester-button" style="width: 60px" type="button">All</button>
-						<button id="spring17" class="admin__head-semester-button" type="button">Spring '17</button>
-						<button id="winter16" class="admin__head-semester-button" type="button">Winter '16</button>
-					</div>-->
+    <div class="admin container">
+		<div class="columns">
+			<div class="admin__main column is-10">
+				<div class="admin__head">
+					<div class= "columns is-mobile is-gapless is-marginless">
+						<!--<div class= "column is-8">
+							<div class= "admin__head-semester">
+								<button id="all" class="admin__head-semester-button" style="width: 60px" type="button">All</button>
+								<button id="spring17" class="admin__head-semester-button" type="button">Spring '17</button>
+								<button id="winter16" class="admin__head-semester-button" type="button">Winter '16</button>
+							</div>
+						</div>-->
+						<div class= "column is-4">
+							<div class= "admin-genre-selection">
+								<p>Genre: &nbsp </p>
+								<el-select v-model="currentGenre" filterable placeholder="Select" style="width: 210px;">
+									<el-option v-for="genre in genres" :key="genre.name" :label="genre.name" :value="genre.name">
+									</el-option>
+								</el-select>
+							</div>
+						</div>
+					</div>
 				</div>
-				<div class= "column is-4">
-					<div class= "admin-genre-selection">
-						<p>Genre: &nbsp </p>
-						<el-select v-model="currentGenre" filterable placeholder="Select" style="width: 210px;">
-							<el-option v-for="genre in genres" :key="genre.name" :label="genre.name" :value="genre.name">
-							</el-option>
-						</el-select>
+
+				<div class="admin__featured">
+
+					<div class="ftdcard" v-for="v in videos" v-show="v.genre === currentGenre" v-bind:key="v.id">
+						<router-link class="ftdcard-thumbnail" :to="'/video/' + v.id" tag="a">
+							<img :src="v.thumb" alt="video-thumbnail" class="ftdcard-thumbnail-image">
+						</router-link>
+						<div class="ftdcard-content">
+							<div class="ftdcard-details">
+								<p class="ftdcard-description">{{ v.title }}</p>
+								<p class="ftdcard-semester">{{ v.class }}</p>
+								<p class="ftdcard-date">{{ v.presentedAt | sliceDate }}</p>
+							</div>
+							<div class="ftdcard-foot">
+								<span class="ftdcard-genre">
+									<i class="fa fa-commenting-o" aria-hidden="true"></i> 
+									{{ v.genre }}
+								</span>
+							</div>				
+						</div>
+					</div>
+					
+					<!-- <upload-video></upload-video> -->
+
+				</div>
+
+				<div class="admin__class">
+					<div class="classcards fx-dir-col">
+						<div class="classcard fx-dir-row" v-for="v in videos" v-bind:key="v.id">
+							<router-link :to="'/video/' + v.id"  tag="a" class="class-card columns is-gapless">
+								<div class="class-card-image column is-2 is-gapless is-marginless">
+									<img :src="v.thumb" alt="video-thumbnail" class="class-card-image-thumbnail">
+								</div>
+								<div class="class-card-description column is-7 is-gapless is-marginless has-text-left">
+									<p class= "class-card-description-title">{{ v.title }}</p>
+									<p class= "class-card-description-details">Duration: | Class: {{ v.class }}</p>
+								</div>
+								<div class="class-card-genre column is-3 is-gapless is-marginless">
+									<strong class="class-card-genre-color">{{ v.genre }}</strong> &nbsp&nbsp<i class="fa fa-book fa-2x" aria-hidden="true"></i>
+								</div>
+							</router-link>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
 
-		<div class="admin__content">
-
-			<div class="videocard" v-for="v in videos" v-show="v.genre === currentGenre" v-bind:key="v.id">
-				<router-link class="videocard-thumbnail" :to="'/video/' + v.id" tag="a">
-					<img :src="v.thumb" alt="video-thumbnail" class="videocard-thumbnail-image">
-				</router-link>
-				<div class="videocard-content">
-					<div class="videocard-details">
-						<p class="videocard-description">{{ v.title }}</p>
-						<p class="videocard-semester">{{ v.class }}</p>
-						<p class="videocard-date">{{ v.presentedAt | sliceDate }}</p>
-					</div>
-					<div class="videocard-foot">
-						<span class="videocard-genre">
-							<i class="fa fa-commenting-o" aria-hidden="true"></i> 
-							{{ v.genre }}
-						</span>
-					</div>				
+			<!-- <div class="admin__sidebar column is-2">
+				<div class="container">
+					<ul class="sidebar-menu">
+						<li><a class="sidebar-menu__link">Home</a></li>
+						<li><a class="sidebar-menu__link">Class 1</a></li>
+						<li><a class="sidebar-menu__link">Class 2</a></li>
+						<li><a class="sidebar-menu__link">Class 3</a></li>
+					</ul>
 				</div>
-			</div>
-			
-			<upload-video></upload-video>
+			</div> -->
 
+			<aside class="menu admin__sidebar column is-2">
+				<ul class="menu-list">
+					<li><a>Metalogon Home</a></li>
+					<li><a>Materials Science and Engineering</a></li>
+					<li><a>Aeronastro Class</a></li>
+					<li><a>Mathematics Class</a></li>
+				</ul>
+			</aside>
 		</div>
-
 	</div>
 
 </template>
+
+
 
 <script>
 	import axios from 'axios'
@@ -89,39 +132,7 @@
 </script>
 
 <style>
-    .v-videos {
-        display: flex;
-    }
-        .v-video {
-            display: flex;
-            flex-direction: column;
-            padding-right: 54px;
-        }
 
-        .video-thumb {
-            width: 100%;
-        }
-
-        .tabs {
-            margin: 0;
-            padding: 0;
-            margin-top: 30px;
-        }
-            .tabs ul {
-                padding: 0;
-                margin: 0
-            }
-
-                li.is-active a {
-                    color: #6B6B6B !important;
-                }
-
-
-/* ==============================================
-                #admin-PAGE
-	================================================= */
-
-/*-------------- CARD ---------------- */
 .admin__head {
 	min-width: 770px;
 	width: 100%;
@@ -153,75 +164,101 @@
 		line-height: 60px;
 	}
 
-.admin__content {
+
+
+.admin__featured {
 	display: flex;
 	flex-direction: row;
 	flex-wrap: wrap;
 	justify-content: flex-start;
 	min-width: 770px;
 	width: 100%;
-	height: 100%;
+	height: 50%;
 }
 
-
-
-	.videocard {
+	.ftdcard {
 		height:auto;
-		width: calc(100% /3 - 20px);
+		width: 350px; /* calc(100% /3 - 20px) */
 		margin: 10px;
 		border-radius: 3px;
 		border: 1px solid rgba(170, 178, 200, 0.25);
 	}	
-		.videocard-video-thumbnail{
+		.ftdcard-video-thumbnail{
 			width: 100%;
 		}
 
-		.videocard-content {
-			padding: 25px 25px 5px 25px;
+		.ftdcard-content {
+			padding: 15px 15px 5px 15px;
 		}
 
-			.videocard-details {
+			.ftdcard-details {
 			}
-				.videocard-description {
+				.ftdcard-description {
 					font-size: 1em;
 					padding: 0;
 					margin: 0;
 				}
 			
-				.videocard-semester,
-				.videocard-date {
+				.ftdcard-semester,
+				.ftdcard-date {
 					font-size: 0.8em;
 					padding: 0;
 					margin: 0;
 				}
 
-			.videocard-foot {
+			.ftdcard-foot {
 				border-top: 1px solid rgba(170, 178, 200, 0.25);
 				margin-top: 30px;
 				padding-top: 10px;
 				font-size: 0.8em;
 				display: flex;
 			}
-				.videocard-genre {
+				.ftdcard-genre {
 					width: 50%;
 				}
-				.videocard-effectiveness {
+				.ftdcard-effectiveness {
 					width: 50%;
 					display: flex;
 					justify-content: space-between;
 					align-items: center;
 				}
 
-					.videocard-effectiveness-bar {
+					.ftdcard-effectiveness-bar {
 					  background-color: red !important;
 					}
 
-					.videocard-effectiveness-label {
+					.ftdcard-effectiveness-label {
 					}
-					
-		
 
+
+
+
+
+/* ==============================================
+                #ADMIN-FEATURED
+	================================================= */	
+
+	.admin__class {
+		height: 50%;
+	}
+
+
+
+
+
+/* ==============================================
+                #ADMIN-SIDEBAR
+	================================================= */
+
+	.admin__sidebar {
+	}
 			
+			.sidebar-menu__link {
+				color: #000;
+			}
+
+
+
 
 /* ==============================================
                 #ANNOTATION-BADGE
